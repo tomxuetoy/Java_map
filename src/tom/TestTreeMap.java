a5772cf23d0dd040aa57bb734df931ad686c384a
Tom Xue <tomxue0126@gmail.com>
2012-11-22 12:30:35 +0800

    to show how HashMap and TreeMap work

---
+++ src/tom/TestTreeMap.java
@@ -0,0 +1,37 @@
+package tom;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class TestTreeMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// HashMap是无序的
+		prt("The following is HashMap");
+		HashMap<String, Object> hashMap = new HashMap<String, Object>();
+		hashMap.put("004", new Integer(40));
+		hashMap.put("003", new Integer(30));
+		hashMap.put("001", new Integer(10));
+		hashMap.put("002", new Integer(20));
+
+		prt(hashMap);
+		// TreeMap是有序的
+		prt("The following is TreeMap");
+		TreeMap<String, Object> treeMap = new TreeMap<String, Object>();
+		treeMap.put("004", new Integer(40));
+		treeMap.put("003", new Integer(30));
+		treeMap.put("001", new Integer(10));
+		treeMap.put("002", new Integer(20));
+
+		prt(treeMap);
+
+	}
+
+	private static void prt(Object obj) {
+		System.out.println(obj);
+	}
+
+}
